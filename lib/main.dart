05d86b0f945a49cbb5f96ea8f34091a29db77df8
Md. Amanur Rahman <amanur.rahman@egeneration.co>
2020-02-20 00:13:33 +0600

    db list added

--- lib/main.dart
+++ lib/main.dart
@@ -94,3 +94,157 @@ class _DbSplashScreenState extends State<DbSplashScreen> {
     );
   }
 }
+
+
+/*
+  deviceInformation() async {
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    DatabaseReference reference = FirebaseDatabase.instance.reference();
+    var data = {
+      "model": '${androidInfo.model}',
+      "androidId": '${androidInfo.androidId}',
+      "manufacturer": '${androidInfo.manufacturer}',
+      "id": '${androidInfo.id}',
+      "board": '${androidInfo.board}',
+      "bootloader": '${androidInfo.bootloader}',
+      "brand": '${androidInfo.brand}',
+      "version_release": '${androidInfo.version.release}',
+      "version_baseOS": '${androidInfo.version.baseOS}',
+      "version_codename": '${androidInfo.version.codename}',
+      "device": '${androidInfo.device}',
+      "display": '${androidInfo.display}',
+      "fingerprint": '${androidInfo.fingerprint}',
+      "host": '${androidInfo.host}',
+      "hashCode": '${androidInfo.hashCode}',
+      'created_at': '${createTime}'
+    };
+    var db = FirebaseDatabase.instance
+        .reference()
+        .child("user_device_information")
+        .child('${androidInfo.androidId}')
+        .reference();
+    db.once().then((DataSnapshot snapshot) {
+      Map<dynamic, dynamic> values = snapshot.value;
+
+      if (values == null) {
+        reference
+            .child('user_device_information')
+            .child('${androidInfo.androidId}')
+            .set(data);
+      }
+    });
+
+    var dbcontact = {
+      'name': 'Amanur Rahman',
+      'mobile': '01719839141',
+      'designation': 'Fronted Developer',
+      'address': 'Dhaka, Bangladesh',
+      'email': 'kanaks2010@gmail.com',
+    };
+
+    var dbpolice = FirebaseDatabase.instance
+        .reference()
+        .child("db_police_contact")
+        .child('${androidInfo.androidId}')
+        .reference();
+
+    dbpolice.once().then((DataSnapshot snapshot) {
+      Map<dynamic, dynamic> values = snapshot.value;
+      var date = new DateTime.now().millisecondsSinceEpoch;
+      */
+class ContactList {
+  String name, mobile, designation, address, email;
+
+  ContactList(this.name, this.mobile, this.designation, this.address, this.email);
+
+}
+
+List<ContactList> contactLists = [];
+
+
+//DBlist
+/*
+@override
+void initState() {
+  super.initState();
+  // DatabaseReference contacts = FirebaseDatabase.instance.reference().child('db_police_contact');
+
+  */
+/*contacts.once().then((DataSnapshot snap){
+      var KEYS = snap.value.keys;
+      var DATA = snap.value;
+      contactLists.clear();
+
+      for(var individualKey in KEYS){
+        ContactList contactList = new ContactList (
+            DATA[individualKey]['name'],
+            DATA[individualKey]['mobile'],
+            DATA[individualKey]['designation'],
+            DATA[individualKey]['address'],
+            DATA[individualKey]['email']
+        );
+        contactLists.add(contactList);
+      }
+
+      setState(() {
+        print('Contact ' + '${contactLists.length}');
+        print('Contact ' + '${contactLists[0].email}');
+      });
+    });*//*
+
+}
+*/
+
+
+/*
+Widget newWidget (){
+  ListView.builder(
+    itemCount: 10,
+    itemBuilder: (context, i) {
+      return Column(
+        children: <Widget>[
+          ListTile(
+            leading: CircleAvatar(
+              child: Text('H'),
+            ),
+            title: Text(
+              'Md. Amanur Rahman',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black),
+            ),
+            subtitle: Text(
+              '+8801719839141',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black54),
+            ),
+            trailing: CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 15,
+              child: GestureDetector(
+                onTap: ()=>{
+                  print('Detected'),
+                },
+                child: Text('i', style:
+                TextStyle(color: Colors.teal, fontWeight: FontWeight.w400),),
+              ),
+            ),
+            onTap: () => {
+              print('taped'),
+              launch("tel://21213123123")
+            },
+            onLongPress: () => {
+              print('onLongPress'),
+            },
+          ),
+          Divider(
+            thickness: 1,
+          )
+        ],
+      );
+    },
+  ),
+}*/
